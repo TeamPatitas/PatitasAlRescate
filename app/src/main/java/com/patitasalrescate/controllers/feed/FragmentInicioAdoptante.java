c1d421e72f5759cfb09d5882c44e1dd511d3f5b6
Galaxy <74384334+GalaxyM4@users.noreply.github.com>
2026-07-02 10:02:16 -0500

    minor changes

--- app/src/main/java/com/patitasalrescate/controllers/feed/FragmentInicioAdoptante.java
+++ app/src/main/java/com/patitasalrescate/controllers/feed/FragmentInicioAdoptante.java
@@ -27,7 +27,7 @@ public class FragmentInicioAdoptante extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        updateTitle("Inicio");
+
         if (getArguments() != null) {
 //            mParam1 = getArguments().getString(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
@@ -37,7 +37,7 @@ public class FragmentInicioAdoptante extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        updateTitle("Inicio");
         return inflater.inflate(R.layout.fg_inicio_adoptante, container, false);
     }
 
